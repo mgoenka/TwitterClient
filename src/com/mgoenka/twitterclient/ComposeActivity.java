6378dd1ba7d2e77c44bb8e9a6a13bb11364cb9ee
Mohit Goenka <mgoenka@yahoo-inc.com>
2014-02-02 16:18:14 -0800

    Check the character limit for posting tweets

--- src/com/mgoenka/twitterclient/ComposeActivity.java
+++ src/com/mgoenka/twitterclient/ComposeActivity.java
@@ -4,8 +4,12 @@ import org.json.JSONArray;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.mgoenka.twitterclient.controllers.TwitterClientApp;
@@ -16,6 +20,9 @@ public class ComposeActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_compose);
+		EditText etComposedTweet = (EditText) findViewById(R.id.etComposedTweet);
+		Button btnTweet = (Button) findViewById(R.id.btnTweet);
+		watcher(etComposedTweet, btnTweet);
 	}
 	
 	public void onCancel(View v) {
@@ -36,4 +43,30 @@ public class ComposeActivity extends Activity {
 		setResult(RESULT_OK);
 		finish();
 	}
+	
+	protected void watcher(final EditText etComposedTweet,final Button btnTweet) {
+	    etComposedTweet.addTextChangedListener(new TextWatcher() {
+	        public void afterTextChanged(Editable s) { 
+	            if (etComposedTweet.length() == 0) {
+	            	btnTweet.setEnabled(false);
+	            } else if (etComposedTweet.length() > 140) {
+	            	btnTweet.setEnabled(false);
+	            	Toast.makeText(ComposeActivity.this, getString(R.string.length_exceeded), Toast.LENGTH_SHORT).show();
+	            } else {
+	            	btnTweet.setEnabled(true);
+	            }
+	        }
+
+	        public void beforeTextChanged(CharSequence s, int start, int count, int after){
+	        }
+	        
+	        public void onTextChanged(CharSequence s, int start, int before, int count){
+	        }
+	    }); 
+	    
+	    if (etComposedTweet.length() == 0) {
+	    	btnTweet.setEnabled(false);
+	    }
+	}
+
 }
